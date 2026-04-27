ce6c5986b091d8d42e055d5cecf13135b585770f
Andrew <rnkel301007@gmail.com>
2026-04-27 15:48:14 +0300

    Stage 1

---
+++ LAB_7ASM/LAB_7ASM/LAB_7ASM.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "rus");
+    int a, b, c, d, e, y;
+    printf("Введите a, b, c, d, e: ");
+    scanf_s("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    __asm {
+        mov eax, b      
+        imul c         
+        idiv d          
+        mov ebx, a      
+        sub ebx, eax    
+        mov ecx, e      
+        add ebx, ecx    
+        mov y, ebx      
+    }
+
+    printf("y = %d", y);
+
+    return 0;
+}
